c78cbb1c89aa58197b098c0f39a1d7a46d226fe5
Farnabaz <farnabaz@gmail.com>
2023-08-22 17:16:32 +0200

    docs: init docs

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,46 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Nuxt MDC',
+    description: 'MDC stands for MarkDown Components. This syntax supercharges regular Markdown to write documents interacting deeply with any Vue component.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      github: 'nuxtlabs/nuxt-mdc',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
       }
     },
+
     github: {
-      dir: '.starters/default/content',
+      dir: 'docs/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'nuxt-mdc',
+      owner: 'nuxtlabs',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
-      fluid: true
+      fluid: false
     },
+
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
+      fluid: false,
+      title: 'Nuxt MDC'
+    },
+
+    titleTemplate: '%s · Nuxt MDC'
   }
 })
