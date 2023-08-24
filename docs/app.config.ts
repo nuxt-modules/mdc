5f16e4ea6dab2c36479fcb417c3f3bb31e4fb3d4
Farnabaz <farnabaz@gmail.com>
2023-08-24 16:04:14 +0200

    docs: update cover image

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
     title: 'Nuxt MDC',
-    description: 'MDC stands for MarkDown Components. This syntax supercharges regular Markdown to write documents interacting deeply with any Vue component.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    description: 'MDC stands for Markdown Components. This syntax supercharges regular Markdown to write documents interacting deeply with any Vue component.',
+    image: 'https://nuxt-mdc.nuxt.space/cover.png',
 
     socials: {
       twitter: 'nuxt_js',
