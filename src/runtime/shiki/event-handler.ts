e3f42080f18035b36a2e08ff5c6ddad0c828ade1
nobkd <44443899+nobkd@users.noreply.github.com>
2023-09-05 13:43:42 +0200

    fix: hand over highlights

--- src/runtime/shiki/event-handler.ts
+++ src/runtime/shiki/event-handler.ts
@@ -4,14 +4,14 @@ import { useShikiHighlighter } from './highlighter'
 import type { Theme, TokenStyleMap } from './types'
 
 export default eventHandler(async (event) => {
-  const { code, lang, theme: themeString } = getQuery(event) // where are the highlights?
+  const { code, lang, theme: themeString, highlights } = getQuery(event)
   const theme = JSON.parse(themeString as string)
 
   const shikiHighlighter = useShikiHighlighter({})
 
   const styleMap: TokenStyleMap = {}
 
-  const {tree, className } = await shikiHighlighter.getHighlightedAST(code as string, lang as Lang, theme as Theme, { styleMap }) // missing highlights in options
+  const { tree, className } = await shikiHighlighter.getHighlightedAST(code as string, lang as Lang, theme as Theme, { styleMap, highlights: highlights as number[] })
 
   return {
     tree,
