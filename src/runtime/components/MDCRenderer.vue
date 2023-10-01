12668e13530c4239bb9d89ce6e2446cb84411f7d
Maxime Pauvert <maximepvrt@gmail.com>
2023-10-01 22:36:33 +0200

    feat: add default value to variable binding

--- src/runtime/components/MDCRenderer.vue
+++ src/runtime/components/MDCRenderer.vue
@@ -168,8 +168,9 @@ function renderBinding (node: MDCElement, h: CreateElement, documentMeta: MDCDat
     }
     return {}
   }, data)
+  const defaultValue = node.props?.defaultValue;
 
-  return h(Text, value)
+  return h(Text, value ?? defaultValue ?? '')
 }
 
 /**
