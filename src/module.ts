e4ca6fcdc3a7e8b8de6d9d767ca7f2afb017edc3
Pooya Parsa <pooya@pi0.io>
2024-01-18 21:30:06 +0100

    fix: add unwasm support to nitro prerenderer (#126)

--- src/module.ts
+++ src/module.ts
@@ -69,18 +69,23 @@ export default defineNuxtModule<ModuleOptions>({
       // Enable unwasm for shikiji
       nuxt.hook('ready', () => {
         const nitro = useNitro()
-        if (!nitro.options.experimental?.wasm) {
-          nitro.options.externals = nitro.options.externals || {}
-          nitro.options.externals.inline = nitro.options.externals.inline || []
-          nitro.options.externals.inline.push(id => id.endsWith('.wasm'))
-          nitro.hooks.hook('rollup:before', async (_, rollupConfig) => {
+        const addWasmSupport = (_nitro: typeof nitro) => {
+          if (nitro.options.experimental?.wasm) { return }
+          _nitro.options.externals = _nitro.options.externals || {}
+          _nitro.options.externals.inline = _nitro.options.externals.inline || []
+          _nitro.options.externals.inline.push(id => id.endsWith('.wasm'))
+          _nitro.hooks.hook('rollup:before', async (_, rollupConfig) => {
             const { rollup: unwasm } = await import('unwasm/plugin')
             rollupConfig.plugins = rollupConfig.plugins || []
             ;(rollupConfig.plugins as any[]).push(unwasm({
-              ...nitro.options.wasm as any,
+              ..._nitro.options.wasm as any,
             }))
           })
         }
+        addWasmSupport(nitro)
+        nitro.hooks.hook('prerender:init', (prerenderer) => {
+          addWasmSupport(prerenderer)
+        })
       })
 
       // Add server handlers
