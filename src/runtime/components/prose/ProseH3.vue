6a717352ac2b2a538203b4cfe38324a645426033
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-05-17 10:10:57 +0200

    fix: add `true` as accepted value for `headings.anchorLinks` option

--- src/runtime/components/prose/ProseH3.vue
+++ src/runtime/components/prose/ProseH3.vue
@@ -16,5 +16,5 @@ import { computed, useRuntimeConfig } from '#imports'
 const props = defineProps<{ id?: string }>()
 
 const { headings } = useRuntimeConfig().public.mdc
-const generate = computed(() => props.id && headings?.anchorLinks?.h3)
+const generate = computed(() => props.id && (headings?.anchorLinks === true || headings?.anchorLinks?.h3))
 </script>
