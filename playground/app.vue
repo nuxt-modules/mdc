8a7c5b5dcc757fa455d8870d748b22a4e05174b8
Farnabaz <farnabaz@gmail.com>
2024-09-12 12:08:43 +0200

    chore: update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="grid grid-cols-2 h-screen">
-    <textarea
-      v-model="md"
-      class="w-full p-4"
-    />
-    <MDC
-      v-slot="{ data, body }"
-      :value="md"
-    >
-      <article class="p-4 prose">
-        <h1 v-if="data?.title">
-          {{ data.title }}
-        </h1>
-        <MDCRenderer
-          v-if="body"
-          :body="body"
-          :data="data"
-        />
-      </article>
-    </MDC>
+  <div class="">
+    <div class="p-1 flex gap-1">
+      <UButton v-for="(example, name) in examples" :key="name" size="xs" @click="md = example">
+        {{ name }}
+      </UButton>
+    </div>
+    <div class="grid grid-cols-3 h-screen">
+      <textarea
+        v-model="md"
+        class="w-full p-4 bg-gray-600 font-mono text-sm"
+      />
+      <MDC
+        v-slot="{ data, body }"
+        :value="md"
+      >
+        <pre class="h-screen overflow-auto bg-gray-700 text-xs">{{ body }}</pre>
+        <article class="p-4 prose prose-invert bg-gray-900">
+          <h1 v-if="data?.title">
+            {{ data.title }}
+          </h1>
+          <MDCRenderer
+            v-if="body"
+            :body="body"
+            :data="data"
+          />
+        </article>
+      </MDC>
+    </div>
   </div>
 </template>
 
@@ -27,9 +35,8 @@ import { useDark, useLocalStorage } from '@vueuse/core'
 
 useDark()
 
-const key = 'nuxt-mdc-playground-code'
-const md = useLocalStorage(key,
-  `---
+const examples = {
+  default: `---
 title: Sam
 ---
 
@@ -51,7 +58,17 @@ async function main(mdc: string) {
   return ast // [!code --]
 }
 \`\`\`
-`)
+`,
+  table: `| Item              | In Stock | Price |
+| :---------------- | :------: | ----: |
+| Python **Hat**    |   True   | 23.99 |
+| SQL Hat           |   True   | 23.99 |
+| Codecademy Tee    |  False   | 19.99 |
+| Codecademy Hoodie |  False   | 42.99 |`
+}
+
+const key = 'nuxt-mdc-playground-code'
+const md = useLocalStorage(key, examples.default)
 </script>
 
 <style>
