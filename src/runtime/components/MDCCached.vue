27c28fde2fbb9ae49f2f2e4e5e8f594397773045
Chris Towles <Chris.Towles@gmail.com>
2025-04-24 23:28:11 -0400

    fix(cache): fix issue where value watch could get out of sync

--- src/runtime/components/MDCCached.vue
+++ src/runtime/components/MDCCached.vue
@@ -108,7 +108,7 @@ const body = computed(() => props.excerpt ? data.value?.excerpt : data.value?.bo
 
 watch(() => props.value, () => {
   refresh()
-})
+}, { immediate: true })
 
 // Simple string hashing function
 function hashString(str: string | object) {
